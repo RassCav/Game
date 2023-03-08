a8c5b38e59350300a14f1cbe1ff0a3d7c93b0802
RassCav <hamza.siad.sh@hotmail.com>
2023-03-08 15:56:32 +0100

    fix bugs

--- test_dices.py
+++ test_dices.py
@@ -1,10 +1,10 @@
 import unittest
 
-def test_1(self):
+def test_fun_1(self):
         res = int(test.roll())
         assert res >= 1 and res <= 6  
         
-def test_2(self):
+def test__fun_2(self):
         res = test.roll_again
         assert isinstance(res, str) == True
         
@@ -12,4 +12,5 @@ def test_3(self):
         assert min_val == 1 and max_val == 6
 
 
-    
+if __name__ == '__main__':
+    unittest.main()    
