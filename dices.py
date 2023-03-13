a8f661bd2df108df7aba331224c962b99337b698
RassCav <hamza.siad.sh@hotmail.com>
2023-03-13 22:58:55 +0100

    Fix Errors

--- dices.py
+++ dices.py
@@ -22,8 +22,6 @@ while roll_again == "yes" or roll_again == "y":
     roll_again = input("Roll the Dices Again? : ")
     if roll_again == "no" or "n":
         break
-""""
-""""
 
 if __name__ == '__main__':
     unittest.main()
