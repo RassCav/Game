b637d4431181a3c3f6e0104fcd59aab0e0ab560e
RassCav <hamza.siad.sh@hotmail.com>
2023-03-13 22:51:24 +0100

    delete class "test"

--- dices.py
+++ dices.py
@@ -1,22 +1,21 @@
 import random
 import unittest
 
-class test:
-    global min_val, max_val
-    min_val = 1
-    max_val = 6
-    roll_again = "yes"
+global min_val, max_val
+min_val = 1
+max_val = 6
+roll_again = "yes"
     
-    def roll():
-        return random.randint(min_val, max_val)
+def roll():
+    return random.randint(min_val, max_val)
 
-    while roll_again == "yes" or roll_again == "y":
-        print("Rolling The Dices...")
-        print("The Values are :")
-        print(roll())
-        roll_again = input("Roll the Dices Again? : ")
-        if roll_again == "no" or "n":
-            break
+while roll_again == "yes" or roll_again == "y":
+    print("Rolling The Dices...")
+    print("The Values are :")
+    print(roll())
+    roll_again = input("Roll the Dices Again? : ")
+    if roll_again == "no" or "n":
+        break
         
 """"
 if __name__ == '__main__':
